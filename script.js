2e0b324b56833fdb77050d49b7f7aca471f5904a
Robson G. <123911094+RobsonSantos3795@users.noreply.github.com>
2025-11-05 17:45:01 -0300

    Armazenamentos de novos tipos de dados.
    Novos armazenamento de dados para otimização do projeto.

---
+++ script.js
@@ -0,0 +1,48 @@
+let tarefas = [];
+function adicionarTarefa() {
+    let inputTarefa = document.getElementById("inputTarefa");
+    let tarefa = inputTarefa.value.trim();
+
+    inputTarefa.value = "";
+
+    if (tarefa == "") {
+        let mensagemErro = "Insira uma tarefa válida, por favor!";
+        document.getElementById("mensagem").textContent = mensagemErro;
+    } else {
+        let mensagemValida = "Tarefa adicionada com sucesso!";
+        document.getElementById("mensagem").textContent = mensagemValida;
+        tarefas.push(tarefa);
+        renderizarTarefa();
+        
+    }
+}
+function renderizarTarefa() {
+    let listaTarefa = document.getElementById("listaTarefa");
+    listaTarefa.innerHTML = "";
+
+    for (let i = 0; i < tarefas.length; i = i + 1) {
+        let novaTarefa = document.createElement("li");
+
+        novaTarefa.textContent = tarefas[i];
+
+        let botaoRemover = document.createElement("button");
+        botaoRemover.className = "remover";
+        botaoRemover.textContent = "Remover";
+        botaoRemover.onclick = () => removerBotao(i);
+
+        novaTarefa.appendChild(botaoRemover);
+        listaTarefa.appendChild(novaTarefa);
+    }
+}
+function removerBotao(i) {
+    tarefas.splice(i, 1);
+    renderizarTarefa();
+}
+function entrou() {
+    let cor = document.getElementById("cor");
+    cor.style.background = "yellow";
+}
+function saiu() {
+    let cor = document.getElementById("cor");
+    cor.style.background = "greenyellow";
+}
